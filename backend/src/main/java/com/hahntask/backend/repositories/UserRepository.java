bc1b94ad3cd34f37790171cff5a2ea6d11636bed
MouadBNL <mouadbnl.2k01@gmail.com>
2025-06-21 18:16:29 +0100

    feat: created user repository

---
+++ backend/src/main/java/com/hahntask/backend/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.hahntask.backend.repositories;
+
+import com.hahntask.backend.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
