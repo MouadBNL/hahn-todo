5cd7007cba6250dc26f5f689c13ae2e49567a2a8
MouadBNL <mouadbnl.2k01@gmail.com>
2025-06-21 17:47:51 +0100

    feat: created TaskService with implementation

--- backend/src/main/java/com/hahntask/backend/services/TaskService.java
+++ backend/src/main/java/com/hahntask/backend/services/TaskService.java
@@ -3,7 +3,16 @@ package com.hahntask.backend.services;
 import com.hahntask.backend.domain.entities.Task;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface TaskService {
     List<Task> getAll();
+
+    Task find(UUID uuid);
+
+    Task create(Task task);
+
+    Task update(UUID uuid, Task task);
+
+    void delete(UUID uuid);
 }
--- backend/src/main/java/com/hahntask/backend/services/impl/TaskServiceImpl.java
+++ backend/src/main/java/com/hahntask/backend/services/impl/TaskServiceImpl.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -17,5 +18,29 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public List<Task> getAll() {
         return taskRepository.findAll();
+    }
+
+    @Override
+    public Task find(UUID uuid) {
+        return taskRepository.findById(uuid).orElse(null);
+    }
+
+    public Task create(Task task) {
+        return taskRepository.save(task);
+    }
+
+    @Override
+    public Task update(UUID uuid, Task task) {
+        var existing = taskRepository.findById(uuid).orElseThrow();
+        existing.setTitle(task.getTitle());
+        existing.setDescription(task.getDescription());
+        existing.setPriority(task.getPriority());
+        existing.setCompletedAt(task.getCompletedAt());
+        return taskRepository.save(existing);
+    }
+
+    @Override
+    public void delete(UUID uuid) {
+        taskRepository.deleteById(uuid);
     }
 }
