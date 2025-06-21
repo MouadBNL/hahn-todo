14aedd204ccbc23e1c5e7d13e741798e91d52fb1
MouadBNL <mouadbnl.2k01@gmail.com>
2025-06-21 16:03:12 +0100

    feat: setup task entity

---
+++ backend/src/main/java/com/hahntask/backend/domain/entities/Task.java
@@ -0,0 +1,53 @@
+package com.hahntask.backend.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(unique = true, nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name= "priority", nullable = false)
+    private TaskPriority priority = TaskPriority.NONE;
+
+    @Column(name="completed_at")
+    private LocalDateTime completedAt = null;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
---
+++ backend/src/main/java/com/hahntask/backend/domain/entities/TaskPriority.java
@@ -0,0 +1,8 @@
+package com.hahntask.backend.domain.entities;
+
+public enum TaskPriority {
+    NONE,
+    LOW,
+    MEDIUM,
+    HIGH
+}
